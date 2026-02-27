008f897ad99f8aa2cab8489ff8b89ec0b030c827
yourdaybylucas <yourdaybylucas@gmail.com>
2026-02-27 17:31:21 -0500

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,1 +1,26 @@
+import { MetadataRoute } from 'next';
 
+export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = 'https://www.yourdaybylucas.com';
+
+  return [
+    {
+      url: baseUrl,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 1,
+    },
+    {
+      url: `${baseUrl}/collections`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/about`,
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.5,
+    },
+  ];
+}
